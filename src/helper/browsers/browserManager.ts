90a85589854f27aa87bacfc50007f13ec8d1fd2c
koteswara cheetirala <cheetirala@gmail.com>
2026-01-28 11:08:44 +0000

    updated headless to true to get it working in github actions

--- src/helper/browsers/browserManager.ts
+++ src/helper/browsers/browserManager.ts
@@ -1,6 +1,6 @@
 import { chromium, firefox, LaunchOptions, webkit } from '@playwright/test';
 const options: LaunchOptions = {
-    headless: false, args: [
+    headless: true, args: [
         '--disable-features=PasswordLeakDetection,PasswordManagerOnboarding'
     ]
 }
@@ -19,10 +19,10 @@ export const invokeBrowser = () => {
             return chromium.launch({ ...options, channel: 'msedge' });
 
         case "firefox":
-            return firefox.launch({ headless: false });
+            return firefox.launch({ headless: true });
 
         case "webkit":
-            return webkit.launch({ headless: false });
+            return webkit.launch({ headless: true });
 
         default:
             throw new Error("Please use | chrome | edge | firefox | webkit");
